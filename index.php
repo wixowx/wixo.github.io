dc87680d2fec4700015550bfe37f51da503c063e
Wixo Group <67395047+wixowx@users.noreply.github.com>
2022-08-29 04:38:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,23 @@
-<?php
-
-$con = mysqli_connect('remotemysql.com', 'tjwuFq57pY', 'O3stac5WDq');
-mysqli_query($con,"SET NAMES 'utf8'");
-mysqli_select_db($con, 'tjwuFq57pY');
-
-
-$Query_tag   = "SELECT * FROM `star` WHERE star_id";
-$Execute_tag = mysqli_query($con,$Query_tag);
-$row_tag     = mysqli_fetch_assoc($Execute_tag); 
-
-$name = $row_tag['star_name'];
-
-echo $name ;
+<html>
+<head>
+    <meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Wixo Gameo</title>
+	
+	<!-- Allow fullscreen mode on iOS devices. (These are Apple specific meta tags.) -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<meta name="HandheldFriendly" content="true" />
+	<!-- Chrome for Android web app tags -->
+	<meta name="mobile-web-app-capable" content="yes" />
+	<link rel="shortcut icon" sizes="200x200" href="asset/icon-256.png" />
+	<link rel="stylesheet" href="asset/style.css">
+</head>
+<body>
+   <div class="center">
+       <img src="asset/icon-256.png">
+       <h3>Gameo Powred By Wixo Group</h3>
+   </div>
+</body>
+</html>
