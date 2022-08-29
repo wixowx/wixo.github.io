9f683e729bff2966475bde867849ea6aca8fb267
Wixo Group <67395047+wixowx@users.noreply.github.com>
2022-08-29 04:37:21 +0200

    Create zx.php

---
+++ zx.php
@@ -0,0 +1,15 @@
+<?php
+
+$con = mysqli_connect('remotemysql.com', 'tjwuFq57pY', 'O3stac5WDq');
+mysqli_query($con,"SET NAMES 'utf8'");
+mysqli_select_db($con, 'tjwuFq57pY');
+
+
+$Query_tag   = "SELECT * FROM `star` WHERE star_id";
+$Execute_tag = mysqli_query($con,$Query_tag);
+$row_tag     = mysqli_fetch_assoc($Execute_tag); 
+
+$name = $row_tag['star_name'];
+
+echo $name ;
+?>
