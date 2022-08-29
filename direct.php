a4e9004716f7bc59f5f7c8343089ff58d7352cde
Wixo Group <67395047+wixowx@users.noreply.github.com>
2022-08-29 04:34:38 +0200

    Update direct.php

--- direct.php
+++ direct.php
@@ -1,13 +1,24 @@
-<?php
-if(isset($_GET['code'])) {
-$code    = $_GET['code'];
-$title   = $_GET['title'];
-$file   = base64_decode($code); 
-$name = 'iegybest.in--'.$title.'.mp4';
-header("Content-Description: File Transfer"); 
-header("Content-Type: application/octet-stream"); 
-header("Content-Disposition: attachment; filename=\"". basename($name) ."\""); 
-readfile ($file);
-exit(); 
-}
-?>
+
+<html>
+<head>
+    <meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title>Wixo Gameo</title>
+	
+	<!-- Allow fullscreen mode on iOS devices. (These are Apple specific meta tags.) -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+	<meta name="HandheldFriendly" content="true" />
+	<!-- Chrome for Android web app tags -->
+	<meta name="mobile-web-app-capable" content="yes" />
+	<link rel="shortcut icon" sizes="200x200" href="asset/icon-256.png" />
+	<link rel="stylesheet" href="asset/style.css">
+</head>
+<body>
+   <div class="center">
+       <img src="asset/icon-256.png">
+       <h3>Gameo Powred By Wixo Group</h3>
+   </div>
+</body>
+</html>
