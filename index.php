59f50b066894d7e5f41ec055829a221770d424d9
Wixo Group <67395047+wixowx@users.noreply.github.com>
2022-09-01 08:26:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ $default_timezone = 'Etc/UTC'; // UTC
 
 // Root path for file manager
 // use absolute path of directory i.e: '/var/www/folder' or $_SERVER['DOCUMENT_ROOT'].'/folder'
-$root_path = $_SERVER['DOCUMENT_ROOT'].'/Files';
+$root_path = $_SERVER['DOCUMENT_ROOT'].'';
 
 // Root url for links in file manager.Relative to $http_host. Variants: '', 'path/to/subfolder'
 // Will not working if $root_path will be outside of server document root
